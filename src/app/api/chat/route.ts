c3ad025c55a9dbc7c1a826b98cb24c268118569f
Pål Andreas Morholmen <pmor@computas.com>
2024-10-14 14:39:13 +0200

    :recycle: Move tools to separate file

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,8 +1,6 @@
-import { createResource } from '@/lib/actions/resources'
 import { openai } from '@ai-sdk/openai'
-import { convertToCoreMessages, streamText, tool } from 'ai'
-import { z } from 'zod'
-import { findRelevantContent } from '@/lib/ai/embedding'
+import { convertToCoreMessages, streamText } from 'ai'
+import { addResource, getInformation } from '@/lib/ai/tools'
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30
@@ -17,23 +15,8 @@ export async function POST(req: Request) {
     Only respond to questions using information from tool calls.
     if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
     tools: {
-      addResource: tool({
-        description: `add a resource to your knowledge base.
-          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
-        parameters: z.object({
-          content: z
-            .string()
-            .describe('the content or resource to add to the knowledge base'),
-        }),
-        execute: async ({ content }) => createResource({ content }),
-      }),
-      getInformation: tool({
-        description: `get information from your knowledge base to answer questions.`,
-        parameters: z.object({
-          question: z.string().describe('the users question'),
-        }),
-        execute: async ({ question }) => findRelevantContent(question),
-      }),
+      addResource,
+      getInformation,
     },
   })
 
